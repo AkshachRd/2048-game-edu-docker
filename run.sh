0d327d9798312522e825ba6f5bd75e2660061f42
Daniil <dr.chashka@gmail.com>
2025-01-29 09:39:47 +0000

    Добавил докер файл. Создал скрипты для запуска и остановки.

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+IMAGE_NAME="2048-game-edu-docker"
+
+docker build -t $IMAGE_NAME .
+
+docker run -d -p 8080:80 --name instance_1 $IMAGE_NAME
+
+docker run -d -p 8081:80 --name instance_2 $IMAGE_NAME
+
+docker ps
---
+++ stop.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+IMAGE_NAME="2048-game-edu-docker"
+
+docker rm instance_1
+
+docker rm instance_2
+
+docker ps
